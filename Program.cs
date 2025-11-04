601141ef767629179be1e8423d48fda2e427e75f
liamg2810 <lgardn3r@gmail.com>
2025-11-04 09:05:53 +0000

    cleanup?

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using AIWellness.Chat;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/query", (string query) =>
+{
+    return Chat.Message(query);
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
