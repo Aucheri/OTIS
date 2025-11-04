75029223a1759cf33208f52dbbd39399fb9503c5
5upern1ce <alfie-t@supern-ice.me>
2025-11-04 14:30:23 +0000

    Add marked dependency and enhance message rendering in Chatbox component

--- frontend/src/lib/Chatbox.svelte
+++ frontend/src/lib/Chatbox.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { tick } from "svelte";
+    import {marked} from "marked"
 
    
     let message = $state("")
@@ -46,12 +47,12 @@
 <div class="grid grid-rows-[1fr_auto] h-screen w-screen p-10 gap-4">
     <div class="flex flex-col overflow-y-auto gap-2  overflow-x-hidden" bind:this={element}>
         {#each messageHistory as message, i}
-            <span class={`${i % 2 ? "bg-neutral-600 after:bg-neutral-600 text-white rounded-bl-none" : "bg-neutral-200 after:bg-neutral-200 self-end rounded-br-none"} w-auto px-4 py-2 rounded-md max-w-3/4 wrap-anywhere relative after:block after:absolute after:right-0 after:bottom-0 after:translate-y-1/2 ${i % 2 ? "after:left-0 after:-translate-x-1/2" : "after:translate-x-1/2"} after:w-4 after:rotate-45 after:aspect-square after:`}>{message}</span>
+            <span class={`${i % 2 ? "bg-neutral-600 after:bg-neutral-600 text-white rounded-bl-none" : "bg-neutral-200 after:bg-neutral-200 self-end rounded-br-none"} w-auto px-4 py-2 rounded-md max-w-3/4 wrap-anywhere relative after:block after:absolute after:right-0 after:bottom-0 after:translate-y-1/2 ${i % 2 ? "after:left-0 after:-translate-x-1/2" : "after:translate-x-1/2"} after:w-4 after:rotate-45 after:aspect-square after:`}>{@html marked(message)}</span>
         {/each}
     </div>
      
     <form onsubmit={SendData} class="flex gap-4">
         <input bind:value={message} class="w-3/4 rounded-lg" placeholder="Send a Chat"/>
-        <button disabled={isSending} type="submit" class="bg-neutral-500 px-4 flex-1 text-white rounded-lg cursor-pointer disabled:bg-neutral-400 disabled:cursor-not-allowed">Send Message</button>
+        <button disabled={isSending} type="submit" class={`bg-neutral-500 px-4 flex-1 text-white rounded-lg cursor-pointer disabled:bg-neutral-400 disabled:cursor-not-allowed ${message.length > 0 ? "bg-blue-400! enabled:hover:bg-blue-500!" : ""} transition-colors duration-100 enabled:hover:bg-gray-600`}>Send Message</button>
     </form>
 </div>
