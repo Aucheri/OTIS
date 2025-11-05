c4ef5b6820b30a1ec209f2587fa5d21b2e898420
Liam Gardner <114190936+liamg2810@users.noreply.github.com>
2025-11-05 14:57:49 +0000

    Revert "Made BAT file better"

--- api-and-frontend-startup.bat
+++ api-and-frontend-startup.bat
@@ -1,4 +1,4 @@
-start powershell -noexit -command "dotnet restore; dotnet run"
+start powershell -noexit -command "dotnet run"
 cd frontend
-start powershell -noexit -command "npm install; npm run dev"
+start powershell -noexit -command "npm run dev"
 echo "Opened"
